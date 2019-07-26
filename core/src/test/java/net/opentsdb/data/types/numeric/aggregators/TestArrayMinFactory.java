5fd64429bb78a683eefe81338866b640ca8e41f3
Chris Larsen <clarsen@verizonmedia.com>
2019-07-25 17:24:30 -0700

    CORE: - Fix the ArraySumFactory where it would return a 0 even if we only had a single   time series with a NaN in it.

--- core/src/test/java/net/opentsdb/data/types/numeric/aggregators/TestArrayMinFactory.java
+++ core/src/test/java/net/opentsdb/data/types/numeric/aggregators/TestArrayMinFactory.java
@@ -158,7 +158,7 @@ public class TestArrayMinFactory {
     agg.accumulate(new double[] { 3, Double.NaN, 5, -1 });
     assertArrayEquals(new double[] { 3, Double.NaN, 5, -1 }, agg.doubleArray(), 0.001);
   }
- 
+  
   @Test
   public void nanThenReals() throws Exception {
     ArrayMinFactory.ArrayMin agg = new ArrayMinFactory.ArrayMin(false);
